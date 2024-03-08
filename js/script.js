950b41abf522e11b19d615410f3061351c4b6b2c
Gabrielesuma <gabrielesuma139@gmail.com>
2024-03-08 17:35:50 +0100

    struttura html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-let fizzBuzz = 'FizzBuzz'
+const squareWrapper = document.createElement('div');
 
-for(let i = 1; i <=100; i++){
+squareWrapper.className = 'd-flex justify-content-between align-content-center flex-wrap';
+squareWrapper.setAttribute('id', 'boxes');
+
+let numBox = 100;
+let tmpHtml = '';
+
+
+
+for(let i = 1; i <=numBox; i++){
     if(i % 3 === 0 && i % 5 === 0){
-        console.log(fizzBuzz);
+        tmpHtml += `<div class="box">FizzBuzz</div>`;
+    } else if(i % 3 === 0){
+        tmpHtml += `<div class="box">Fizz</div>`;
+    } else if(i % 5 === 0){
+        tmpHtml += `<div class="box">Buzz</div>`;
+    } else {
+        tmpHtml += `<div class="box">${i}</div>`;
     }
-    console.log(i);
 }
+
+squareWrapper.innerHTML = tmpHtml;
+const container = document.querySelector('.container');
+container.append(squareWrapper);
